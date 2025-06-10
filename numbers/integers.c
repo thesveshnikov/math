080a0a224e3b1b93d4320bb0f4c28e692d39892f
Sveshnikov <esbarros@proton.me>
2025-06-10 13:00:03 -0300

    Create integers.c

---
+++ numbers/integers.c
@@ -0,0 +1,23 @@
+#include "integers.h"
+#include <stdio.h>
+
+void integers(void) {
+    int StartOfSequence;
+    int EndOfSequence;
+
+    printf("Enter where the sequence begins: ");
+    scanf("%d", &StartOfSequence);
+
+    printf("Enter where the sequence ends: ");
+    scanf("%d", &EndOfSequence);
+
+    if (StartOfSequence > EndOfSequence) {
+        printf("Error: %d is greater than %d.\n", StartOfSequence, EndOfSequence);
+    } else if (StartOfSequence == EndOfSequence) {
+        printf("Error: %d is equal to %d.\n", StartOfSequence, EndOfSequence);
+    } else {
+        for (int i = StartOfSequence; i <= EndOfSequence; i++) {
+            printf("%d ", i);
+        }
+    }
+}
